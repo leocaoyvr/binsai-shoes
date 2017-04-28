34a75dddb9fa241054666be0f14cf17eec967acb
Leo Cao <leo@ilite.me>
2017-04-27 19:00:22 -0700

    working on submenu

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -11,3 +11,24 @@ $(function() {
     arrows: false
   });
 });
+
+// Nav Sub Menu
+$(function() {
+  $('.nav-item.has-sub').mouseenter(function() {
+    $(this).find('.nav-sub').addClass('active');
+    $(this).parents('.nav').addClass('js-show-submenu');
+    $(this).parents('.nav').find('.nav-sub-bg').addClass('fadeInDown');
+  });
+
+    $('.nav-item.has-sub').mouseleave(function() {
+    $(this).find('.nav-sub').removeClass('active');
+    $(this).parents('.nav').removeClass('js-show-submenu');
+    $(this).parents('.nav').find('.nav-sub-bg').removeClass('fadeInDown');
+  });
+
+  $('.nav-item.has-sub').click(function() {
+    $(this).find('.nav-sub').toggleClass('active');
+    $(this).parents('.nav').toggleClass('js-show-submenu');
+    $(this).parents('.nav').find('.nav-sub-bg').toggleClass('fadeInDown');
+  });
+});
